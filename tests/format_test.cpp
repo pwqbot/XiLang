cd8ddaf6f0d64ce6c9f93fade6f35f010c72c0ac
pwqbot <ding00000804@gmail.com>
2022-12-06 20:18:56 +0800

    test: add format test

---
+++ tests/format_test.cpp
@@ -0,0 +1,46 @@
+#include <catch2/catch_test_macros.hpp>
+#include <compiler/ast.h>
+#include <compiler/ast_format.h>
+#include <compiler/utils.h>
+
+namespace xi {
+
+TEST_CASE("Test Xi_Integer") {
+    auto result = Xi_Integer{123};
+    REQUIRE(fmt::format("{}", result) == "Xi_Integer 123");
+
+    auto result2 = Xi_Integer{-123};
+    REQUIRE(fmt::format("{}", result2) == "Xi_Integer -123");
+}
+
+TEST_CASE("Test Xi_Real") {
+    auto result = Xi_Real{123.456};
+    REQUIRE(fmt::format("{}", result) == "Xi_Real 123.456");
+}
+
+TEST_CASE("Test Xi_Boolean") {
+    auto result = Xi_Boolean{true};
+    REQUIRE(fmt::format("{}", result) == "Xi_Boolean true");
+
+    auto result2 = Xi_Boolean{false};
+    REQUIRE(fmt::format("{}", result2) == "Xi_Boolean false");
+}
+
+TEST_CASE("Test Xi_String") {
+    auto result = Xi_String{"hello"};
+    REQUIRE(fmt::format("{}", result) == "Xi_String \"hello\"");
+}
+
+TEST_CASE("Test recursive_wrapper") {
+    auto result = recursive_wrapper{Xi_Integer{123}};
+    REQUIRE(fmt::format("{}", result) == "Xi_Integer 123");
+}
+
+TEST_CASE("Test Xi_Binop") {
+    auto result = Xi_Binop{Xi_Integer{123}, Xi_Integer{456}, Xi_Op::Add};
+    REQUIRE(fmt::format("{}", result) == "Xi_Binop +\n"
+                                         "\tXi_Integer 123\n"
+                                         "\tXi_Integer 456");
+}
+
+} // namespace xi
