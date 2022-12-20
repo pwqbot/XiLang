78888b9a944cb4d6b74953837c7431c39624eab3
pwqbot <ding00000804@gmail.com>
2022-12-21 04:22:30 +0800

    feat: read input file from cmd & integrate clang

--- app/compiler/main.cpp
+++ app/compiler/main.cpp
@@ -3,15 +3,29 @@
 #include <compiler/ast/type_format.h>
 #include <compiler/generator/llvm.h>
 #include <compiler/parser/program.h>
+#include <cstdlib>
+#include <gflags/gflags.h>
+#include <spdlog/spdlog.h>
 
-int main()
+DEFINE_string(o, "a", "Output file");
+
+int main(int argc, char *argv[])
 {
-    std::string_view input_file = "app/compiler/test.xi";
-    std::string_view output_obj = "app/compiler/test.o";
-    std::string_view output_ir  = "app/compiler/test.ir";
-    std::string      input_string;
+    gflags::ParseCommandLineFlags(&argc, &argv, true);
+
+    if (argc < 2)
+    {
+        spdlog::error("No input file specified");
+        return 1;
+    }
+
+    std::string input_file = argv[1];
+    std::string output_obj = FLAGS_o + ".o";
+    std::string output_ll  = FLAGS_o + ".ll";
+
+    std::string input_string;
     // read the whole file into a string
-    std::ifstream file(input_file.data());
+    std::ifstream file(input_file);
     input_string.assign(
         std::istreambuf_iterator<char>(file), std::istreambuf_iterator<char>()
     );
@@ -22,13 +36,13 @@ int main()
     {
         if (ast_result.value().second == "\n")
         {
-            fmt::print("Parse successfully\n");
-            fmt::print("AST:\n {}\n", ast_result.value().first);
+            spdlog::info("Parse successfully\n");
+            spdlog::info("AST:\n {}\n", ast_result.value().first);
             auto ast      = ast_result.value().first;
             auto ast_type = TypeAssign(ast);
             if (!ast_type.has_value())
             {
-                fmt::print(
+                spdlog::error(
                     "Type error {}\n {}\n",
                     ast_type.error().what(),
                     ast_type.error().node
@@ -36,22 +50,38 @@ int main()
                 return 1;
             }
 
-            fmt::print("Type:\n {}\n", ast_type.value());
+            spdlog::info("Type:\n {}\n", ast_type.value());
             auto codegen_result = xi::CodeGen(ast);
             if (!codegen_result)
             {
-                fmt::print("LLVM: \n {}\n", codegen_result.error().what());
+                spdlog::error("LLVM: \n {}\n", codegen_result.error().what());
                 return 1;
             }
             // write the IR to a file
-            std::ofstream ir_file(output_ir.data());
+            spdlog::info("Generate LLVM IR to {}", output_ll);
+            std::ofstream ir_file(output_ll.data());
             ir_file << codegen_result.value();
 
             xi::GenObj(output_obj);
+
+            // use clang to link the object file
+            spdlog::info("Linking to {}", FLAGS_o);
+            auto clang_result = std::system(
+                fmt::format("clang {} -o {}", output_obj, FLAGS_o).c_str()
+            );
+            if (clang_result != 0)
+            {
+                // delete the object file
+                std::remove(output_obj.data());
+                spdlog::error("Linking failed");
+                return 1;
+            }
+            std::remove(output_obj.data());
+            spdlog::info("Linking successfully");
         }
         else
         {
-            fmt::print(
+            spdlog::error(
                 "Parse fail!\nparsed: {}\n{} not parsed\n",
                 ast_result.value().first,
                 ast_result.value().second
@@ -60,7 +90,7 @@ int main()
     }
     else
     {
-        fmt::print("Failed to parse\n");
+        spdlog::error("Failed to parse\n");
         return 1;
     }
     return 0;
